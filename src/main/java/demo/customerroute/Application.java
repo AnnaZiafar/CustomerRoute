c3fa95f9d4c9839832ab37858efdd151b9df4f5e
Anna Ziafar <annaaziafar@gmail.com>
2026-04-05 12:13:14 +0200

    Initial commit

---
+++ src/main/java/demo/customerroute/Application.java
@@ -0,0 +1,17 @@
+package demo.customerroute;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        Dotenv env = Dotenv.configure().load();
+        env.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+        SpringApplication.run(Application.class, args);
+    }
+
+}
