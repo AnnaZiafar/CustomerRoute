316de0fb57ef21fb9ad719cdac6a30bfed2b0ef3
Anna Ziafar <annaaziafar@gmail.com>
2026-05-28 16:55:03 +0200

    Initial commit

---
+++ src/test/java/demo/messageroute/MessageRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.messageroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageRouteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
