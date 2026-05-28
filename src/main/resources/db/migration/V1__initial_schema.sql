57c835219a740e2f6ca61d4454a32ff33a90d5d5
Anna Ziafar <annaaziafar@gmail.com>
2026-05-28 18:08:41 +0200

    Add docker support for mysql db and artemis mq
    Initialise db with flyway. Include 3 tiers

---
+++ src/main/resources/db/migration/V1__initial_schema.sql
@@ -0,0 +1,10 @@
+create table tier (
+    id bigint auto_increment primary key,
+    level varchar(50) unique not null,
+    discount_percentage smallint not null,
+);
+
+insert into tier values
+(null, 'base', 0),
+(null, 'gold', 30),
+(null, 'silver', 10)
